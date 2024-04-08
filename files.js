d188d2e7a673d1b49fbd0e78d629ae0d42b6ad49
devgirlpro <agalidari@gmail.com>
2024-04-08 18:00:15 +0200

    some basics abot file system

--- files.js
+++ files.js
@@ -5,14 +5,43 @@ fs.readFile('./docs/blog1.txt', (err, data) => {
   if (err) {
     console.log(err);
   }
-  console.log(data.toString())
+  console.log(data.toString());
 });
 
+console.log('last line');
 
-console.log('last line')
+//writing files is asynchronous
+fs.writeFile('./docs/blog1.txt', 'Good evening ccccccccc', () => {
+  console.log('file was written');
+});
 
-//writing files
+fs.writeFile('./docs/blog2.txt', 'Good evening ', () => {
+  console.log('file was written');
+});
 
 //directories
+if (!fs.existsSync('./assets')) {
+  fs.mkdir('./assets', (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('folder Created');
+  });
+}else {
+    fs.mkdir('./assets', (err) => {
+        if(err) {
+            console.log(err)
+        }
+        console.log('Folder deleted')
+    })
+}
 
 //deleting files
+if(fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        if(err) {
+            console.log(err)
+        }
+        console.group('file deleted')
+    })
+}
