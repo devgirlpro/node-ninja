4ec22ce276aabbe0814b7d945fd4df8ed91e01ec
devgirlpro <agalidari@gmail.com>
2024-04-09 00:29:15 +0200

    send dummy response

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.method, req.url, req.headers)
+    console.log(req.method, req.url, req.headers);
+
+    //set header content type
+    res.setHeader('Content-type', 'text/html');
+
+    res.write('<h2>Helo from server</h2>');
+    res.write('<h3>welcome to my club</h3>');
+    res.end();
 })
 
 server.listen(3001, 'localhost', () => {
