ed432c2cbc0534473f85ced359c057eaa5984db3
devgirlpro <agalidari@gmail.com>
2024-04-08 15:46:47 +0200

    setTimeout & setInterval

--- global.js
+++ global.js
@@ -2,5 +2,11 @@
 
 
 global.setTimeout(() => {
-    console.log('in the timeout')
+    console.log('in the timeout');
+    clearInterval(int);
 }, 3000);
+
+
+const int = setInterval(() => {
+    console.log('in the interval')
+}, 1000)
