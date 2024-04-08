dc11f1576368f88eafd28f9356f3cdf47ed8cd80
devgirlpro <agalidari@gmail.com>
2024-04-08 15:49:55 +0200

    dirname and filename

--- global.js
+++ global.js
@@ -1,12 +1,17 @@
 // console.log(global)
 
 
-global.setTimeout(() => {
-    console.log('in the timeout');
-    clearInterval(int);
-}, 3000);
+// global.setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000);
 
 
-const int = setInterval(() => {
-    console.log('in the interval')
-}, 1000)
+// const int = setInterval(() => {
+//     console.log('in the interval')
+// }, 1000)
+
+//__dirname
+console.log("dirname", __dirname)
+//__filename
+console.log("falename=>", __filename)
