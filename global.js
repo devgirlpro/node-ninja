e37605f469ae40b92ece5d8b0e41d9fefcf6b7c0
devgirlpro <agalidari@gmail.com>
2024-04-08 16:28:15 +0200

    import paople.js into modules.js

--- global.js
+++ global.js
@@ -1,17 +1,19 @@
-// console.log(global)
+console.log(global)
 
 
-// global.setTimeout(() => {
-//     console.log('in the timeout');
-//     clearInterval(int);
-// }, 3000);
+global.setTimeout(() => {
+    console.log('in the timeout');
+    clearInterval(int);
+}, 3000);
 
 
-// const int = setInterval(() => {
-//     console.log('in the interval')
-// }, 1000)
+const int = setInterval(() => {
+    console.log('in the interval')
+}, 1000)
 
 //__dirname
 console.log("dirname", __dirname)
+
 //__filename
 console.log("falename=>", __filename)
+
---
+++ modules.js
@@ -0,0 +1,6 @@
+const names = require('./people');
+
+// const {names, ages} = require('./people')
+
+
+ console.log(names)
