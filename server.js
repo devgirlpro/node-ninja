65b9cbb014998778f9679169dc90fbde8fd1937b
devgirlpro <agalidari@gmail.com>
2024-04-09 14:57:32 +0200

    have a html file as a response anlso with diffrent url

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.method, req.url, req.headers);
+    //console.log(req.method, req.url, req.headers);
 
     //set header content type
     res.setHeader('Content-type', 'text/html');
 
-    res.write('<h2>Helo from server</h2>');
-    res.write('<h3>welcome to my club</h3>');
-    res.end();
+    let path = './views/';
+
+    switch(req.url) {
+        case '/':
+          path += 'index.html';
+        //   res.statusCode = 200;
+          break;
+        case '/about':
+          path += 'about.html';
+        //   res.statusCode = 200;
+          break;
+        // case '/about-us':
+        //   res.statusCode = 301;
+        //   res.setHeader('Location', '/about');
+        //   res.end();
+        //   break;
+        default:
+          path += '404.html';
+          res.statusCode = 404;
+      }
+
+  // send html
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    //res.write(data);
+    res.end(data);
+  });
+
+   
 })
 
 server.listen(3001, 'localhost', () => {
