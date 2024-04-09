d135f45b7fd7a232f04ac9ff336034eb08241412
devgirlpro <agalidari@gmail.com>
2024-04-09 15:30:06 +0200

    redirecting url

--- server.js
+++ server.js
@@ -10,19 +10,23 @@ const server = http.createServer((req, res) => {
     let path = './views/';
 
     switch(req.url) {
+
         case '/':
           path += 'index.html';
-        //   res.statusCode = 200;
+          res.statusCode = 200;
           break;
+
         case '/about':
           path += 'about.html';
-        //   res.statusCode = 200;
+          res.statusCode = 200;
           break;
-        // case '/about-us':
-        //   res.statusCode = 301;
-        //   res.setHeader('Location', '/about');
-        //   res.end();
-        //   break;
+//redirect about-me url to about 
+        case '/about-me':
+          res.statusCode = 301;
+          res.setHeader('Location', '/about');
+          res.end();
+          break;
+
         default:
           path += '404.html';
           res.statusCode = 404;
