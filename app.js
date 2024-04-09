0afddc44670c415ada15a933485f4f2226208b8f
devgirlpro <agalidari@gmail.com>
2024-04-09 18:21:40 +0200

    using EJS as a view engine

--- app.js
+++ app.js
@@ -3,28 +3,33 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs')
+
 //listen gor request
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>Home page</p>');
-    res.sendFile('./views/index.html', { root:__dirname });
-
+    res.render('index')
 })
 
 
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root:__dirname });
+    res.render('about')
 })
 
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+    res.render('create')
 })
+
+//redirect a url to another
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about')
+// })
 
 
 //404 Page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root:__dirname })
+    res.status(404).render('404')
 })
